a40b38edfd89b1235e2649a9584b44153cbbbc50
Mani <as4897616@gmail.com>
2026-02-15 15:10:08 +0530

    Form components

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -24,8 +24,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
 
     let board;
 
-    try {
-        throw new Error("balbala");
+    try { 
         board = await db.board.create({
             data: {
                 title,
